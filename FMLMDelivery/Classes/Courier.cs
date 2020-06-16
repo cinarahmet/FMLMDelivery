623c205cdc2b0b2eef5e04936fa6b7fae9f71153
Çağrı Doğuş İyican <cagri.iyican@trendyol.com>
2020-06-16 14:52:34 +0300

    Mahalle and Courier Added

---
+++ FMLMDelivery/Classes/Courier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMLMDelivery.Classes
+{
+    public class Courier
+    {
+        private readonly string _Id;
+
+        private List<Mahalle> _mahalle_list=new List<Mahalle>();
+
+        private List<Double> _demand_from_mahalle=new List<Double>();
+
+        private Double _total_demand=new double();
+
+        public Courier(String ıd)
+        { 
+            _Id = ıd;
+        }
+
+        public void Set_Total_Demand(Double addition)
+        {
+            _total_demand = _total_demand + addition;
+        }
+
+        public void Add_Mahalle_To_Courier(Mahalle mahalle)
+        {
+            _mahalle_list.Add(mahalle);
+        }
+
+        public void Add_To_Total(Double addition)
+        {
+            _total_demand = _total_demand + addition;
+        }
+    }
+}
--- FMLMDelivery/Classes/Mahalle.cs
+++ FMLMDelivery/Classes/Mahalle.cs
@@ -11,12 +11,31 @@ namespace FMLMDelivery.Classes
 
         private Double _mahalle_demand;
 
-        public Mahalle(String mahalle_id, Double mahalle_demand )
+        private bool _big_mahalle;
+
+        private double Threshold = 120;
+        public Mahalle(String mahalle_id, Double mahalle_demand)
         {
             _mahalle_id = mahalle_id;
-            _mahalle_demand = mahalle_demand;            
+            _mahalle_demand = mahalle_demand;
+            if (_mahalle_demand >= 120)
+            {
+                _big_mahalle = true;
+            }
+            else
+            {
+                _big_mahalle = false;
+            }
         }
 
-        public String Return()
+        public String Return_Mahalle_Id()
+        {
+            return _mahalle_id;
+        }
+
+        public void Set_Remaning_Demand(Double extracted)
+        {   
+            _mahalle_demand = (_mahalle_demand - extracted);
+        }        
     }
 }
